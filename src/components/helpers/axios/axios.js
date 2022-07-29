d8dc453c0f4534f582df38870f77b61b92fe1105
AmritpalChera <cheraamritpal@gmail.com>
2022-07-29 03:43:14 -0400

    in progress

--- src/components/helpers/axios/axios.js
+++ src/components/helpers/axios/axios.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 // import { appError } from '../functions/general';
 
-const CHAT_BASE = process.env.NODE_ENV === 'production' ? 'https://chat-dot-amurse.uk.r.appspot.com' : 'http://localhost:5001';
-const USER_BASE =  process.env.NODE_ENV === 'production'? 'https://user-dot-amurse.uk.r.appspot.com/api' : 'http://localhost:5002/api';
+// const CHAT_BASE = process.env.NODE_ENV === 'production' ? 'https://chat-dot-amurse.uk.r.appspot.com' : 'http://localhost:5001';
+// const USER_BASE = process.env.NODE_ENV === 'production' ? 'https://user-dot-amurse.uk.r.appspot.com/api' : 'http://localhost:5002/api';
+
+
+const CHAT_BASE = 'http://localhost:5001';
+const USER_BASE =   'http://localhost:5002/api';
+
+
 
 const instanceCreater = (baseUrl) => {
   let instance = axios.create({
